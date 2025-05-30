2885ca977e2fce99ddfc98a30130b65c18588b72
Supreet Mutsuddi <sureetmutsuddi@gmail.com>
2025-05-30 16:01:08 -0700

    change

--- Diabetes.R
+++ Diabetes.R
@@ -6,6 +6,12 @@ library(tidyverse)
 library(caret)
 library(corrplot)
 
+#Target Variable
+
+table(df$Diabetes_012/ncol(df))
+mean(df$Diabetes_012)
+str(df)
+
 #Correlation
 
 cor_matrix <- cor(df, use = 'complete.obs')
@@ -19,6 +25,62 @@ top_corr <- as.data.frame(as.table(cor_matrix)) %>%
 head(top_corr, 10)
 
 corrplot(cor_matrix, method = 'color', tl.cex = 0.5)
+
+#Split
+
+set.seed(1234)
+
+partition <- createDataPartition(y=as.factor(df$Diabetes_012), p=0.7, 
+                                 list = FALSE)
+df.train <- df[partition,]
+df.test <- df[-partition,]
+
+mean(df.train$Diabetes_012)
+mean(df.test$Diabetes_012)
+
+#Set the controls
+
+ctrl <- trainControl(method = 'repeatedcv', number = 5, repeats = 3)
+
+#Set up the training grid
+
+rf.grid <- expand.grid(mtry=1:21)
+
+#Set up x and y variables
+
+target <- factor(df.train$Diabetes_012)
+predictors <- df.train[,-1]
+
+#Train Random Forest
+
+rf1 <- train(y=target, x=predictors, method = 'rf', ntree=5, importance=TRUE,
+             trControl = ctrl, tuneGrid = rf.grid)
+
+rf1
+
+plot(rf1)
+
+varImp(rf1)
+
+plot(varImp(rf1))
+
+imp2 <- varImp(rf1, scale = FALSE)
+imp_df <- imp2$importance
+str(imp_df)
+top_class1 <- imp_df[order(-imp_df[['1']]), , drop = FALSE]
+head(top_class1, 5)
+top_class2 <- imp_df[order(-imp_df[['2']]), , drop = FALSE]
+head(top_class2, 5)
+top_class0 <- imp_df[order(-imp_df[['0']]), , drop = FALSE]
+head(top_class0, 5)
+
+
+
+
+
+
+
+
 
 
 
