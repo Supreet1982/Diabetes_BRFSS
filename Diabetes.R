8ff5f8c372ef2ca33445878f04219f5e339eb181
Supreet Mutsuddi <sureetmutsuddi@gmail.com>
2025-05-30 18:06:36 -0700

    change

--- Diabetes.R
+++ Diabetes.R
@@ -74,6 +74,46 @@ head(top_class2, 5)
 top_class0 <- imp_df[order(-imp_df[['0']]), , drop = FALSE]
 head(top_class0, 5)
 
+randomForest::varImpPlot(rf1$finalModel)
+rf1
+
+pred <- predict(rf1, newdata = df.test, type = 'raw')
+
+confusionMatrix(pred, as.factor(df.test$Diabetes_012))
+
+table(df.test$Diabetes_012)
+
+
+prop.table(table(df$Diabetes_012))
+
+###############################################################################
+
+#Set up tuning grid for XGBoost
+
+xgb.grid <- expand.grid(max_depth=7, min_child_weight = 1, gamma = 0,
+                        nrounds = c(50, 100, 150, 200, 250),
+                        eta = c(0.001, 0.002, 0.01, 0.02, 0.1),
+                        colsample_bytree = 0.6, subsample = 0.6)
+
+xgb.grid
+
+xgb.ctrl <- trainControl(method = 'cv', number = 5)
+
+set.seed(42)
+
+xgb.tuned <- train(as.factor(Diabetes_012) ~ ., data = df.train,
+                   method = 'xgbTree', trControl=xgb.ctrl,
+                   tuneGrid=xgb.grid)
+
+
+xgb.tuned
+ggplot(xgb.tuned)
+
+xgb.pred <- predict(xgb.tuned, newdata = df.test, type='raw')
+confusionMatrix(xgb.pred, as.factor(df.test$Diabetes_012))
+
+prop.table(table(df.train$Diabetes_012))
+
 
 
 
