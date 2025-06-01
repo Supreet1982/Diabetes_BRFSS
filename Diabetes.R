6ccab52e87237a77a7e90eb6e61561d9087fedd5
Supreet Mutsuddi <sureetmutsuddi@gmail.com>
2025-06-01 11:09:53 -0700

    change

--- Diabetes.R
+++ Diabetes.R
@@ -115,11 +115,89 @@ confusionMatrix(xgb.pred, as.factor(df.test$Diabetes_012))
 prop.table(table(df.train$Diabetes_012))
 
 
+################################################################################
 
+#Class imbalance
 
+table(df$Diabetes_012)
 
+df1 <- df 
+df1$Diabetes_012 <- as.factor(df1$Diabetes_012)
 
+min_count <- min(table(df1$Diabetes_012))
 
+df_balanced <- df1 %>%
+  group_by(Diabetes_012) %>%
+  slice_sample(n = min_count) %>%
+  ungroup()
+
+table(df_balanced$Diabetes_012)
+
+library(DMwR2)
+library(ROSE)
+library(smotefamily)
+
+x <- df1[, -which(names(df1) == 'Diabetes_012')]
+y <- df1$Diabetes_012
+
+smote_result <- SMOTE(x, y, K=5)
+df_smote <- smote_result$data
+df_smote$class <- as.factor(df_smote$class)
+table(df_smote$class)
+
+str(df_smote)
+prop.table(table(df_smote$class))
+
+df_downsampled <- df_smote %>%
+  group_by(class) %>%
+  slice_sample(n = 35346) %>%
+  ungroup()
+
+table(df_downsampled$class)
+
+table(df1$Diabetes_012)
+
+################################################################################
+
+#XGBoost on balanced data
+
+str(df_downsampled)
+
+partition2 <- createDataPartition(y=df_downsampled$class, p=0.7, 
+                                 list = FALSE)
+
+df_downsampled.train <- df_downsampled[partition2,]
+df_downsampled.test <- df_downsampled[-partition2,]
+
+set.seed(41)
+
+xgb.tuned2 <- train(class ~ ., data = df_downsampled.train,
+                   method = 'xgbTree', trControl=xgb.ctrl,
+                   tuneGrid=xgb.grid)
+ggplot(xgb.tuned2)
+
+ggplot(xgb.tuned)
+
+xgb.pred2 <- predict(xgb.tuned2, newdata = df_downsampled.test, type='raw')
+confusionMatrix(xgb.pred2, df_downsampled.test$class)
+
+print(varImp(xgb.pred2))
+
+xgb.tuned2
+
+set.seed(42)
+
+xgb.tuned3 <- train(as.factor(class) ~ ., data = df_downsampled.train,
+                    method = 'xgbTree', trControl=xgb.ctrl,
+                    tuneGrid=xgb.grid)
+
+ggplot(xgb.tuned3)
+xgb.pred3 <- predict(xgb.tuned3, newdata = df_downsampled.test, type = 'raw')
+confusionMatrix(xgb.pred3, as.factor(df_downsampled.test$class))
+
+varImp(xgb.tuned3)
+
+################################################################################
 
 
 
